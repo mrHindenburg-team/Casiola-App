078534c47d24a05763f6360f7f251218f3953d02
Tymur Batulin <batulintimur@gmail.com>
2026-04-14 15:24:26 +0200

    Initial Commit

---
+++ Casiola App/Casiola_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Casiola_AppApp.swift
+//  Casiola App
+//
+//  Created by Tymur Batulin on 14.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Casiola_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
